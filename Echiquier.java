f68e12205abee94509f252ef43d364845d1e8c38
Nostres <no04so255@gmail.com>
2025-05-08 01:02:18 +0200

    feat: :construction: Quelques classes initialisées

---
+++ Echiquier.java
@@ -0,0 +1,34 @@
+public class Echiquier {
+
+
+
+
+    public static int getNombreColonne(char lettre) {
+        return switch (lettre) {
+            case 'a' -> 0;
+            case 'b' -> 1;
+            case 'c' -> 2;
+            case 'd' -> 3;
+            case 'e' -> 4;
+            case 'f' -> 5;
+            case 'g' -> 6;
+            case 'h' -> 7;
+            default -> -1;
+        };
+    }
+
+    public static char getLettreColonne(int index) {
+        return switch (index) {
+            case 0 -> 'a';
+            case 1 -> 'b';
+            case 2 -> 'c';
+            case 3 -> 'd';
+            case 4 -> 'e';
+            case 5 -> 'f';
+            case 6 -> 'g';
+            case 7 -> 'h';
+            default -> '?';
+        };
+    }
+    
+}
