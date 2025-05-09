87f52fce1d8ec39aa4350f6519e55dd06cfa372f
Nostres <no04so255@gmail.com>
2025-05-09 20:25:10 +0200

    test: :fire: Test d'interface commenté

--- Executer.java
+++ Executer.java
@@ -1,28 +1,16 @@
 
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Rectangle;
-import java.awt.TextField;
-import java.awt.Window;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class Executer {
     public static void main(String[] args) {
         String testAffichage2 = "1 |\u001B[47m \u001B[30m♜ \u001B[0m\u001B[0m \u001B[30m♞ \u001B[0m\u001B[47m \u001B[30m♝ \u001B[0m\u001B[0m \u001B[30m♚ \u001B[0m\u001B[47m \u001B[30m♛ \u001B[0m\u001B[0m \u001B[30m♝ \u001B[0m\u001B[47m \u001B[30m♞ \u001B[0m\u001B[0m \u001B[30m♜ \u001B[0m|\n2 |\u001B[0m \u001B[30m♟ \u001B[0m\u001B[47m \u001B[30m♟ \u001B[0m\u001B[0m \u001B[30m♟ \u001B[0m\u001B[47m \u001B[30m♟ \u001B[0m\u001B[0m \u001B[30m♟ \u001B[0m\u001B[47m \u001B[30m♟ \u001B[0m\u001B[0m \u001B[30m♟ \u001B[0m\u001B[47m \u001B[30m♟ \u001B[0m\u001B[0m|\n3 |\u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   |\n4 |\u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m|\n5 |\u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   |\n6 |\u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m   \u001B[47m   \u001B[0m|\n7 |\u001B[47m \u001B[37m♙ \u001B[0m\u001B[0m \u001B[37m♙ \u001B[0m\u001B[47m ♙ \u001B[0m ♙ \u001B[47m ♙ \u001B[0m ♙ \u001B[47m ♙ \u001B[0m ♙ |\n8 |\u001B[0m ♖ \u001B[47m ♘ \u001B[0m ♗ \u001B[47m ♕ \u001B[0m ♔ \u001B[47m ♗ \u001B[0m ♘ \u001B[47m ♖ \u001B[0m|\n    A  B  C  D  E  F  G  H";
 
-
+        /*  APP TEST
         System.out.println(testAffichage2);
         System.out.println("Queen: \\u265B.\\n");
 
 
         JFrame frame = new JFrame("Jeu d'échec");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 
         JPanel panel = new JPanel();
@@ -62,7 +50,7 @@ public class Executer {
         frame.setResizable(true);
 
         frame.setBounds(0, 0, 500, 500);
-        Window win = new Window(frame);
+        Window win = new Window(frame);*/
 
         Partie.nouvellePartie();
 
