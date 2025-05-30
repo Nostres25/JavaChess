fb2a8defc4243195dfb42b0522b6b4692905c4f9
Nostres <no04so255@gmail.com>
2025-05-30 22:14:17 +0200

    feat: :construction: Ajout de la classe Reine

---
+++ Reine.java
@@ -0,0 +1,19 @@
+
+public class Reine extends Piece {
+
+    public Reine(Couleur couleur, Case emplacement) {
+        super(emplacement, couleur, this.getClass().getSimpleName(), 'E');
+    }
+
+    public boolean deplacement(Case destination) {
+        int ligneDiff = destination.getLigne() - this.getCase().getLigne();
+        int colonneDiff = destination.getColonne() - this.getCase().Colonne();
+
+        return ligneDiff == 0 || colonneDiff == 0 || Math.abs(ligneDiff) == Math.abs(colonneDiff);
+    }
+
+    @Override
+    public String toString() {
+        return this.getNom() + " " + this.getCouleur() + " ("+this.getCase().getNumero()+")";
+    }
+}
