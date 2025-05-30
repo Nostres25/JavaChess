a43900e972b05cd1dc5f4890dc6f002cdf1cbcfc
Nostres <no04so255@gmail.com>
2025-05-30 20:27:14 +0200

    feat: :construction: Dev de la classe Roi

--- Roi.java
+++ Roi.java
@@ -1,4 +1,18 @@
 
 public class Roi extends Piece {
 
+    public Roi(Couleur couleur, Case emplacement) {
+        super(emplacement, couleur, this.getClass().getSimpleName(), 'R');
+    }
+
+    public boolean deplacement(Case destination) {
+        return 
+            Math.abs(destination.getLigne() - this.getCase().getLigne()) <= 1 ||
+            Math.abs(destination.getColonne() - this.getCase().getColonne()) <= 1;
+    }
+
+    @Override
+    public String toString() {
+        return this.getNom() + " " + this.getCouleur() + " ("+this.getCase().getNumero()+")";
+    }
 }
