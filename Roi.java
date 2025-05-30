aa5e6e006065ea22714df3db1cf842b9f162af60
Nostres <no04so255@gmail.com>
2025-05-30 20:32:30 +0200

    fix: :construction: Correction de du deplacement() de Roi

--- Roi.java
+++ Roi.java
@@ -7,7 +7,7 @@ public class Roi extends Piece {
 
     public boolean deplacement(Case destination) {
         return 
-            Math.abs(destination.getLigne() - this.getCase().getLigne()) <= 1 ||
+            Math.abs(destination.getLigne() - this.getCase().getLigne()) <= 1 &&
             Math.abs(destination.getColonne() - this.getCase().getColonne()) <= 1;
     }
 
