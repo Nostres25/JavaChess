8b84130d7f77501d221df24b2b447a35c850652a
YoussTV <mmenowzz@gmail.com>
2025-05-31 20:50:08 +0200

    Classe Cavalier finie ( encore )

---
+++ Cavalier.java
@@ -0,0 +1,23 @@
+public class Cavalier extends Piece {
+
+    public Cavalier (Couleur couleur, Case emplacement){
+         super(emplacement,couleur,"Cavalier",'C');
+ 
+    }
+
+    public boolean deplacement ( Case destination){
+
+        int colonne = this.getCase().getColonne();
+        int ligne = this.getCase().getLigne();
+
+
+        return
+            Math.abs(colonne - destination.getColonne()) == 2 && Math.abs(ligne - destination.getLigne()) == 1 ||
+            Math.abs(colonne - destination.getColonne()) == 1 && Math.abs(ligne - destination.getLigne()) == 2;
+    
+
+    }
+
+}
+
+    
