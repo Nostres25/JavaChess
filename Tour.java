a839c3ffee05a6becabdcefb07727d20afdd0919
YoussTV <mmenowzz@gmail.com>
2025-05-31 20:35:22 +0200

    Classe Tour développée

--- Tour.java
+++ Tour.java
@@ -1,3 +1,17 @@
-public class Tour {
+public class Tour extends Piece {
+
+    public Tour (Couleur couleur, Case emplacement){
+        super(emplacement,couleur,"Tour",'T');
+    }
+
+     public boolean deplacement ( Case destination){
+
+         return this.getCase().getColonne() == destination.getColonne() || this.getCase().getLigne() == destination.getLigne() ;
+    
+       
+     }
+
+
+
     
 }
