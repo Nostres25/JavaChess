392dcba3ab6477a9a239f4a9349674d4fe80e42b
Nostres <no04so255@gmail.com>
2025-06-01 01:14:39 +0200

    refactor: :bulb: Ajout d'explications concernant l'enum Couleur

--- Couleur.java
+++ Couleur.java
@@ -1,3 +1,9 @@
+// Plus cohérent et plus parlant d'utiliser une énumération plutôt que 
+// - des numéros (int)
+// - des chaînes de caractères
+// - Ou un booléen 
+// qui étaient des solutions possibles pour désigner la couleur ou le tour
+
 public enum Couleur {
     Blanc,
     Noir,
