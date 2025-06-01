3accb9c5a7db33d7548a873c708f243012814147
Nostres <no04so255@gmail.com>
2025-06-01 22:35:20 +0200

    refactor: :bug: Refactor de la gestion d'une partie et correction de bugs

--- Partie.java
+++ Partie.java
@@ -33,12 +33,13 @@ public class Partie {
             System.out.println("Entrez le nom du joueur 2 qui jouera les noirs:");
             String nomJoueur2 = scanner.nextLine();
 
-            scanner.close();
-
-            new Partie(nomJoueur1, nomJoueur2).commencer();
+            System.out.println("- R -> Roi\n- E -> Reine\n- C -> Cavalier\n- F -> Fou\n- T -> Tour\n- P -> Pion ");
+    
+            new Partie(nomJoueur1, nomJoueur2).changerDeTour();
             
         } catch (Exception e) {
-            System.out.println("Une erreur est survenue au lancement de la partie !");
+            System.err.println("Une erreur est survenue au lancement de la partie !");
+            e.printStackTrace();
         }
 
     }
@@ -47,143 +48,165 @@ public class Partie {
         System.out.println("-> " + joueur.getNom() + " (" + joueur.getCouleur() + ") : " + message);
     }
 
-    public void commencer() {
+    public static void erreur(Joueur joueur, String message) {
+        repondre(joueur, "\u001B[31m" + message + "\u001B[0m");
+    }
 
-        System.out.println("- R -> Roi\n- E -> Reine\n- C -> Cavalier\n- F -> Fou\n- T -> Tour\n- P -> Pion ");
+    public static void question(Joueur joueur, String message) {
+        repondre(joueur, "\u001B[33m" + message + "\u001B[0m");
+    }
 
-        Case caseDepart ;
-        Case caseArrivee = null ;
-        boolean enEchec = false;
-
-        while (!this.isFin()) {
-            this.actualiserAffichage();
-
-            System.out.println("Au tour de " + this.joueurActuel.getNom() +" !");
-
-            Scanner scanner = new Scanner(System.in);
-            // Possibilité de jouer d'un coup ou en deux questions
-            repondre(this.joueurActuel, "Que souhaitez-vous jouer ? Par exemple, envoyez a1 pour déplacer la pièce présente sur la case a1. Vous pouvez aussi répondre \"ff\" pour déclarer forfait");
-            
-            try {
-                // Utilisation de "replace" à la place de "trim" pour enlever l'espace entre deux cases
-                // Exemple: " a2 a4 " -> "a2a4"
-                // Il est peu probable qu'un autre caractère invisible soit inséré par l'utilisateur
-                String ligne = scanner.nextLine().replace(" ", "").toUpperCase();
-
-                // Déclarer forfait
-                if (ligne.equals("ff")) {
-                    Joueur joueurAdverse = this.getJoueurAdverse(this.joueurActuel);
-                    repondre(joueurAdverse, this.joueurActuel.getNom() + " veut déclarer forfait, acceptez vous ? (\"oui\" ou \"non\")");
-                    ligne = scanner.nextLine();
-                    if (ligne.equals("oui")) {
-                        this.joueurActuel = this.getJoueurAdverse(this.joueurActuel);
-                        break;
-                    } 
-                }
-
-                // Récupération de la case de départ et de la pièce à jouer
-                repondre(this.joueurActuel, "Quelle pièce voulez-vous déplacer ? (Par exemple, envoyez a1 pour déplacer le pion en a1)");
-                caseDepart = this.getCase(ligne);
-                Piece piece = caseDepart.getPiece();
-
-                // Le joueur doit avoir une pièce à la case sélectionnée
-                if (piece == null || this.joueurActuel.getCouleur() != piece.getCouleur()) {
-                    repondre(this.joueurActuel, "Vous n'avez pas de pièce à la case " + caseDepart.getNumero());
-                    scanner.close();
-                    continue;
-                }
-
-                // Le joueur peut directement préciser la deuxième case pour jouer directement
-                if (ligne.chars().count() > 2) {
-                    caseArrivee = this.getCase(ligne.charAt(2) + ligne.charAt(3) + "");
-                }
-
-                // Le joueur peut aussi jouer en deux questions
-                if (caseArrivee == null) {
-                    repondre(this.joueurActuel, "Vers quelle case voulez-vous déplacer votre " + caseDepart.getPiece().getNom() + " ? (exemple: a4)" );
-                    caseArrivee = this.getCase(scanner.nextLine().replace(" ", ""));
-                }
-
-                // Fin du scanner, la case de départ et la case d'arrivée ont été sélectionnés avec succès.
-                scanner.close();
-
-                if (caseArrivee.equals(caseDepart)) {
-                    repondre(this.joueurActuel, "Vous ne pouvez pas rester sur la même case !");
-                    caseArrivee = null;
-                    continue;
-                }
+    public boolean estDeplacementValide(Piece piece, Case caseArrivee) {
 
                 // Verification du déplacement de la pièce.
                 // La pièce doit pouvoir atteindre la case selon les règles du jeu.
                 if (!piece.deplacement(caseArrivee)) {
-                    repondre(this.joueurActuel, "La pièce " + piece.getNom() + " ne peut pas effectuer un tel déplacement !");
-                    caseArrivee = null;
+                    erreur(this.joueurActuel, "La pièce " + piece.getNom() + " ne peut pas effectuer un tel déplacement !");
                     // TODO peut-être ajouter un piece.getRegle() 
                     // pour un message d'erreur personnalisé en fonction du type de pièce
                     // exemple: Une tour ne peut que se déplacer en ligne droite !
-                    continue;
+                    return false;
                 }
 
                 // Vérification des obstacles et récupération du premier obstacle 
-                Piece obstacle = this.findObstacle(caseDepart, caseArrivee);
+                Piece obstacle = this.findObstacle(piece.getCase(), caseArrivee);
 
                 // Une autre pièce ne doit pas empêcher le déplacement selon les règles du jeu
                 if (obstacle != null) {
-                    repondre(this.joueurActuel, "La piece " + obstacle + " fait obstacle en " + piece.getCase().getNumero() + ".");
-                    caseArrivee = null;
-                    continue;
+                    erreur(this.joueurActuel, "La piece " + obstacle + " fait obstacle en " + obstacle.getCase().getNumero() + ".");
+                    return false;
                 }
 
-                if (estEnEchec(caseDepart, caseArrivee)) {
-                    repondre(this.joueurActuel, "Vous ne pouvez pas effectuer ce déplacement, votre roi serait en echec");
-                    caseArrivee = null;
-                    continue;
+                if (estEnEchec(piece.getCase(), caseArrivee)) {
+                    erreur(this.joueurActuel, "Vous ne pouvez pas effectuer ce déplacement, votre roi serait en echec");
+                    return false;
+                }
+
+                return true;
+    }
+
+    public void demanderAction(boolean enEchec) {
+
+        Scanner scanner = new Scanner(System.in);
+        // Utilisation de "replace" à la place de "trim" pour enlever l'espace entre deux cases
+        // Exemple: " a2 a4 " -> "a2a4"
+        // Il est peu probable qu'un autre caractère invisible soit inséré par l'utilisateur
+        
+        question(this.joueurActuel, "Que souhaitez-vous jouer ? Par exemple, envoyez a1 pour déplacer la pièce présente sur la case a1. Vous pouvez aussi répondre \"ff\" pour déclarer forfait");
+
+        String ligne = scanner.nextLine().replace(" ", "").toUpperCase();
+
+        
+        // Possibilité de jouer d'un coup ou en deux questions
+        
+        try {
+                // Déclarer forfait
+                if (ligne.equals("FF")) {
+                    Joueur joueurAdverse = this.getJoueurAdverse(this.joueurActuel);
+                    question(joueurAdverse, this.joueurActuel.getNom() + " veut déclarer forfait, acceptez vous ? (\"oui\" ou \"non\")");
+                    ligne = scanner.nextLine();
+                    if (ligne.equals("oui")) {
+                        this.joueurActuel = this.getJoueurAdverse(this.joueurActuel);
+                        fin("forfait");
+                        return;
+                    } 
+                }
+
+                // Récupération de la case de départ et de la pièce à jouer
+                Case caseDepart = this.getCase(ligne);
+                Piece piece = caseDepart.getPiece();
+
+                // Le joueur doit avoir une pièce à la case sélectionnée
+                if (piece == null || this.joueurActuel.getCouleur() != piece.getCouleur()) {
+                    erreur(this.joueurActuel, "Vous n'avez pas de pièce à la case " + caseDepart.getNumero());
+                    demanderAction(enEchec);
+                    return;
+                }
+
+                Case caseArrivee = null ;
+
+                // Le joueur peut directement préciser la deuxième case pour jouer directement
+                if (ligne.chars().count() > 2) {
+                    caseArrivee = this.getCase(ligne.charAt(2) +""+ ligne.charAt(3));
+                }
+
+
+                // Le joueur peut aussi jouer en deux questions
+                if (caseArrivee == null) {
+                    question(this.joueurActuel, "Vers quelle case voulez-vous déplacer votre " + caseDepart.getPiece() + " ? (exemple: a4)" );
+                    caseArrivee = this.getCase(scanner.nextLine().replace(" ", "").toUpperCase());
+                }
+
+
+                // TODO Fin du scanner, la case de départ et la case d'arrivée ont été sélectionnés avec succès.
+
+                // Sécurité concernant la possibilité de ne pas se déplacer
+                if (caseArrivee.equals(caseDepart)) {
+                    erreur(this.joueurActuel, "Vous ne pouvez pas rester sur la même case !");
+                    demanderAction(enEchec);
+                    return;
+                }
+
+                if (!this.estDeplacementValide(piece, caseArrivee)) {
+                    demanderAction(enEchec);
+                    return;
                 }
 
                 // Validation du coup et changement de tour
                 this.validerCoup(caseDepart, caseArrivee);
 
-                repondre(this.joueurActuel, "Coup joué : " + piece + " " + caseDepart.getNumero() + " --> " + caseArrivee.getNumero());
+                this.changerDeTour();
 
-                this.joueurActuel = this.getJoueurAdverse(this.joueurActuel);
-
-                enEchec = this.estEnEchec();
-                if (enEchec) {
-                    repondre(this.joueurActuel, "Echec !!");
-                }
-
-            } catch (NoSuchElementException e) {
-                scanner.close();
-                repondre(this.joueurActuel, "Veuillez entrer un numéro de case !");
-                caseArrivee = null;
+             } catch (NoSuchElementException e) {
+                erreur(this.joueurActuel, "Veuillez entrer un numéro de case !");
+                demanderAction(enEchec);
             } catch (IndexOutOfBoundsException e) {
-                scanner.close();
-                repondre(this.joueurActuel, "Veuillez entrer un numéro de case valide !");
-                caseArrivee = null;
+                //TODO cette erreur est survenue anormalement à un moment
+                erreur(this.joueurActuel, "Veuillez entrer un numéro de case valide !");
+                demanderAction(enEchec);
+                e.printStackTrace();
             } catch (NumberFormatException e) {
-                scanner.close();
-                repondre(this.joueurActuel, "Veuillez préciser un chiffre pour localiser la ligne. (et une lettre pour la colonne)");
-                caseArrivee = null;
+                erreur(this.joueurActuel, "Veuillez préciser un chiffre pour localiser la ligne. (et une lettre pour la colonne)");
+                demanderAction(enEchec);
             } catch (Exception e) {
-                scanner.close();
-                System.err.println("Une erreur inattendue est survenue !");
+                System.err.println("\u001B[41mUne erreur inattendue est survenue !\u001B[0m");
                 e.printStackTrace(System.out);
-                caseArrivee = null;
+                demanderAction(enEchec);
             }
 
+    }
+
+    public void changerDeTour() {
+        boolean enEchec = false;
+
+        if (this.joueurActuel == null)  this.joueurActuel = joueurBlanc;
+        else {
+            this.joueurActuel = this.getJoueurAdverse(this.joueurActuel);
+            enEchec = this.estEnEchec(); 
+        }
+                
+        this.actualiserAffichage();
+        if (enEchec) erreur(this.joueurActuel, "\033[1;31mEchec !!\033[0m");
+        System.out.println("Au tour de " + this.joueurActuel.getNom() +" !");
+
+
+        if (isFin()) {
+
+            String raison = "echec";
+            if (enEchec) raison = "Match nul";
+ 
+            fin(raison);
+            return;
         }
 
-        String raison = "echec";
-        if (enEchec) raison = "Match nul"; 
-        this.fin(raison);
-
+        demanderAction(enEchec);
     }
 
     public Joueur getJoueur(Couleur couleur) {
         if (couleur == Couleur.Blanc) {
-            return joueurNoir;
-        } else {
             return joueurBlanc;
+        } else {
+            return joueurNoir;
         }
     }
 
@@ -202,8 +225,8 @@ public class Partie {
     public Case getCase(String numeroCase) throws NumberFormatException, IndexOutOfBoundsException {
         // numeroCase doit être dans le format <LETTRE_COLONNE><numéroLigne>
         // Exemple: "A2" pour la case à la colonne a et sur la ligne 2
-        int colonne = Echiquier.getNombreColonne(numeroCase.charAt(0));
-        int ligne = Integer.parseInt(numeroCase.charAt(1)+"");
+        int colonne = Echiquier.getNombreColonne(numeroCase.charAt(0)) - 1;
+        int ligne = Integer.parseInt(numeroCase.charAt(1)+"") - 1;
 
         return this.echiquier.getCase(colonne, ligne);
     }
@@ -229,8 +252,23 @@ public class Partie {
         int ligneDiff = caseArrivee.getLigne() - caseDepart.getLigne();
         int colonneDiff = caseArrivee.getColonne() - caseDepart.getColonne();
 
-        int ligneDirection = ligneDiff / Math.abs(ligneDiff);
-        int colonneDirection = colonneDiff / Math.abs(colonneDiff);
+        // TODO trouver une meilleure solution
+        int ligneDirection ;
+        if (ligneDiff > 0) {
+            ligneDirection = -1;
+        } else if (ligneDiff < 0) {
+            ligneDirection = 1;
+        } else {
+            ligneDirection = 0;
+        }
+        int colonneDirection ;
+        if (colonneDiff > 0) {
+            colonneDirection = -1;
+        } else if (colonneDiff < 0) {
+            colonneDirection = 1;
+        } else {
+            colonneDirection = 0;
+        }
 
         // Tant que la dernière case (avant case de départ) n'est pas atteinte et tant qu'il n'y a pas d'obstacle
         int ligneDerniereCase = caseDepart.getLigne() - ligneDirection;
@@ -284,14 +322,17 @@ public class Partie {
 
         // Recherche de coup jouable après lequel le roi n'est pas en echec
         for (Case[] ligneCases : this.echiquier.getCases()) {
-            for (Case caseTestee: ligneCases) {
+            for (Case caseTestee : ligneCases) {
 
                 // Vérifier pour toutes les pièces jouables si la case est accessible et sans obstacle
                 for (Piece pieceTestee : this.joueurActuel.getPieces()) {
 
                     if (pieceTestee.deplacement(caseTestee) && this.findObstacle(pieceTestee.getCase(), caseTestee) == null) {
                         // Verifier si le roi est en echec si la pièce testée est déplacée à la case testée
-                        if (!this.estEnEchec(pieceTestee.getCase(), caseTestee)) return false;
+                        if (!this.estEnEchec(pieceTestee.getCase(), caseTestee)) {
+                            System.out.println("isFin() ? -> Un coup est possible avec " + pieceTestee + "en " + caseTestee.getNumero());
+                            return false;
+                        }
                     }
                 }
             }
@@ -302,12 +343,13 @@ public class Partie {
     //TODO Finir
     public void actualiserAffichage() { 
 
-        String affichage = "";
+        String affichage = "    A  B  C  D  E  F  G  H";
         Case[][] cases = this.echiquier.getCases();
-        for (int ligneI = cases.length; ligneI >= 0; ligneI--) {
+        for (int ligneI = cases.length - 1; ligneI >= 0; ligneI--) {
 
             // Écriture d'une nouvelle ligne
-            affichage += "\n" + ligneI+1 + " |";
+            int ligneAffichee = ligneI+1;
+            affichage += "\n" + (ligneAffichee) + " |";
 
             for (int caseI = 0; caseI < cases[ligneI].length; caseI++) {
 
@@ -328,24 +370,32 @@ public class Partie {
                     affichage += " " + contenu + " ";
                 }
             }
+
+            affichage += "\u001B[0m| " + ligneAffichee;
         }
 
-        affichage += "    A   B   C   D   E   F   G   H";
+        affichage += "\n    A  B  C  D  E  F  G  H";
         System.out.println(affichage);
     }
 
-    public void validerCoup(Case caseArrivee, Case caseDepart) {
+    public void validerCoup(Case caseDepart, Case caseArrivee) {
+        Piece pieceDeplacee = caseDepart.getPiece();
         Piece pieceMangee = caseArrivee.getPiece();
+
+        pieceDeplacee.deplacer(caseArrivee);
 
         if (pieceMangee != null) {
             if (!this.getJoueurAdverse(this.joueurActuel).getPieces().remove(pieceMangee)) System.err.println("La pièce mangée n'a pas été retirée de la liste des pièces du joueur adverse");
             // TODO remplir la liste des pieces mangées et retirer le condition
         }
-        caseDepart.getPiece().deplacer(caseArrivee);
 
-        if (caseDepart.getPiece() instanceof Pion pion) {
+        if (pieceDeplacee instanceof Pion pion) {
+            System.out.println(pieceDeplacee + " va bouger : " + pion.aBouge());
             if (!pion.aBouge()) pion.seDeplace();
+            System.out.println(pieceDeplacee + " a bougé : " + pion.aBouge());
         }
+
+        repondre(this.joueurActuel, "Coup joué : " + pieceDeplacee + " " + caseDepart.getNumero() + " --> " + caseArrivee.getNumero());
     }
 
     //TODO Finir
