2f215df03a81450640c861f3ccc999db43b3318c
YoussTV <mmenowzz@gmail.com>
2025-06-02 11:38:33 +0200

    Ajout d'un Override dû à un warning

--- Fou.java
+++ Fou.java
@@ -4,7 +4,7 @@ public class Fou extends Piece {
     public Fou(Couleur couleur) {
         super(couleur, "Fou", 'F');
     }
-
+@Override
     public boolean deplacement(Case destination) {
         int ligneDiff = destination.getLigne() - this.getCase().getLigne();
         int colonneDiff = destination.getColonne() - this.getCase().getColonne();
